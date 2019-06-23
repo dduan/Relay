ec08d7558a8f9782bc0da5c718312c61dcb765e8
Daniel Duan <daniel@duan.ca>
2019-06-22 21:46:34 -0700

    Fix reference to relay

--- Sources/Relay/Relay.swift
+++ Sources/Relay/Relay.swift
@@ -2,7 +2,7 @@ import Combine
 
 /// A publisher that exposes a method for outside callers to publish elements.
 ///
-/// A subject is a publisher that you can use to ”inject” values into a stream, by calling its `send()` method. This can be useful for adapting existing imperative code to the Combine model.
+/// A relay is a publisher that you can use to ”inject” values into a stream, by calling its `send()` method. This can be useful for adapting existing imperative code to the Combine model.
 ///
 /// It's very similar to a subject
 @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
